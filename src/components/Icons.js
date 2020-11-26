9ef3d4c63a2b60ab6fe19020557c2cb4564f49ec
Michael-Lc <michaellc143@gmail.com>
2020-11-26 17:04:33 +0000

    Added the input section to new post component

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,7 +5,7 @@ export function Menu() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      viewBox="0 0 24 24"
+      viewBox="0 0 22 24"
       stroke="currentColor"
     >
       <path
--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -3,9 +3,44 @@ import React from "react";
 import styles from "./styles/NewPost.module.css";
 
 function NewPost() {
+  const openMenu = () => {
+    const menu = document.getElementById("menu");
+    const input = document.getElementById("postContent");
+    menu.style.display = "flex";
+    document.body.classList += " noScroll";
+    input.focus();
+  };
+
+  const closeMenu = () => {
+    const menu = document.getElementById("menu");
+    menu.style.display = "none";
+    document.body.classList.remove("noScroll");
+  };
+
   return (
     <>
-      <button className={styles.newPostBtn}>New Post</button>
+      <button className={styles.newPostBtn} onClick={openMenu}>
+        New Post
+      </button>
+
+      <div className={styles.createPostContainer} id="menu">
+        <div className={styles.topIconsContainer}>
+          <button className={styles.closeBtn} onClick={closeMenu}>
+            &times;
+          </button>
+        </div>
+        <div className={styles.inputContainer}>
+          <form>
+            <div className={styles.formControl}>
+              <textarea
+                className={styles.postInput}
+                id="postContent"
+                placeholder="Type here..."
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
