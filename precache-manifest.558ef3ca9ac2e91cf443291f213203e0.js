eeeb08169c855ed1169bb0245e282cf944aa9e17
Michael-Lc <michaellc143@gmail.com>
2021-02-14 03:22:09 +0000

    Updates

---
+++ precache-manifest.558ef3ca9ac2e91cf443291f213203e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c618f0f300ab85e65123d7140c1c5905",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "80ad5ca8c621fdb6a0bd",
+    "url": "/social-network/static/css/main.0df89a76.chunk.css"
+  },
+  {
+    "revision": "cc7ea6c509f276c2942c",
+    "url": "/social-network/static/js/2.0873cebe.chunk.js"
+  },
+  {
+    "revision": "7c8c498c154c5c7fc14cf0ad148613b4",
+    "url": "/social-network/static/js/2.0873cebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ad5ca8c621fdb6a0bd",
+    "url": "/social-network/static/js/main.c5505a4f.chunk.js"
+  },
+  {
+    "revision": "73dc02b1c42065a1d3d3",
+    "url": "/social-network/static/js/runtime-main.a948e851.js"
+  }
+]);
